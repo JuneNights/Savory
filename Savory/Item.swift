4a1af7a84b609c515adb2adbc243fcd132b57c1b
Jusonit <juniorsj@live.com>
2024-07-15 08:29:08 -0400

    Initial Commit

---
+++ Savory/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Savory
+//
+//  Created by Junior Figuereo on 7/15/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
