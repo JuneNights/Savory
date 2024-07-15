8c6e29b8609587b5e7a3eab80af5859bd8cda6d1
Jusonit <juniorsj@live.com>
2024-07-15 08:32:34 -0400

    Initial

---
+++ Pods/Target Support Files/Pods-Savory-SavoryUITests/Pods-Savory-SavoryUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Savory_SavoryUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Savory_SavoryUITests
+@end
---
+++ Pods/Target Support Files/Pods-SavoryTests/Pods-SavoryTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SavoryTests : NSObject
+@end
+@implementation PodsDummy_Pods_SavoryTests
+@end
